c650917fb346b999bacad0d6df67e135658a29a2
Max Gonzih <gonzih@gmail.com>
2017-03-17 09:17:25 +0100

    Cleanup and fixes

--- src/clj_bst/core.clj
+++ src/clj_bst/core.clj
@@ -6,17 +6,15 @@
 (defn insert [{:keys [key value left right] :as tree} k v]
   (let [node (bst k v)]
     (cond
-    (< key k) (if right
-                (assoc tree :right (insert right k v))
-                (assoc tree :right node))
-    (> key k) (if left
-                (assoc tree :left (insert left k v))
-                (assoc tree :left node)))))
+      (< key k) (assoc tree :right
+                       (if right (insert right k v) node))
+      (> key k) (assoc tree :left
+                       (if left (insert left k v) node)))))
 
 (defn lookup [{:keys [key value left right] :as tree} k]
   (cond
-    (> key k) (when right (lookup right k))
-    (< key k) (when left (lookup left k))
+    (< key k) (when right (lookup right k))
+    (> key k) (when left (lookup left k))
     :else value))
 
 (defn in-order [{:keys [key value left right] :as tree}]
@@ -44,28 +42,25 @@
     (< key k) (when right (assoc tree :right (delete right k)))
     (> key k) (when left  (assoc tree :left  (delete left k)))
     :else (cond
-            (and left right) (let [small (smallest right)
-                                   sk (:key small)
-                                   sv (:value small)
-                                   sright (:right small)]
+            (and left right) (let [{sk :key sv :value sright :right} (smallest right)]
                                {:key sk :value sv :left left :right (delete right sk)})
             left left
             right right
             :else nil)))
 
 #_(-> (bst 15 15)
-    (insert 10 10)
-    (insert 11 11)
-    (insert 20 20)
-    (insert 16 16)
-    (insert 14 14)
-    (insert 22 22)
-    (insert 50 50)
-    ; (lookup 16)
-    ; in-order
-    ; depth
-    ; balanced?
-    (delete 20)
-    ; smallest
-    pprint
-    )
+      (insert 10 10)
+      (insert 11 11)
+      (insert 20 20)
+      (insert 16 16)
+      (insert 14 14)
+      (insert 22 22)
+      (insert 50 50)
+      ; (lookup 16)
+      ; in-order
+      ; depth
+      ; balanced?
+      (delete 20)
+      ; smallest
+      pprint
+      )
